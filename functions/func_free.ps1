1e9e958c59dbdbc2b100552b09b9159fac6214cb
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2023-01-27 14:13:41 +0100

    Add files via upload

--- functions/func_free.ps1
+++ functions/func_free.ps1
@@ -16,16 +16,44 @@
   free
 #>
 
+Function Get-PerformanceCounterLocalName
+{
+  param
+  (
+    [UInt32]
+    $ID,
+	$ComputerName = $env:COMPUTERNAME
+  )
+
+  $code = '[DllImport("pdh.dll", SetLastError=true, CharSet=CharSet.Unicode)] public static extern UInt32 PdhLookupPerfNameByIndex(string szMachineName, uint dwNameIndex, System.Text.StringBuilder szNameBuffer, ref uint pcchNameBufferSize);'
+
+  $Buffer = New-Object System.Text.StringBuilder(1024)
+  [UInt32]$BufferSize = $Buffer.Capacity
+
+  $t = Add-Type -MemberDefinition $code -PassThru -Name PerfCounter -Namespace Utility
+  $rv = $t::PdhLookupPerfNameByIndex($ComputerName, $id, $Buffer, [Ref]$BufferSize)
+
+  if ($rv -eq 0)
+  {
+    $Buffer.ToString().Substring(0, $BufferSize-1)
+  }
+  else
+  {
+    Throw 'Get-PerformanceCounterLocalName : Unable to retrieve localized name. Check computer name and performance counter ID.'
+  }
+}
+
 function free {
     [CmdletBinding()]
     param()
         $totalRam = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).Sum
         [int]$totalMem  = $totalRam/1GB
-        $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
+        $Memory = Get-PerformanceCounterLocalName 4
+        $availMem = (Get-Counter "\$Memory\Available MBytes").CounterSamples.CookedValue
         $freeMem  = $availMem/1000
         [int]$usedMem = $totalMem - $freeMem
         $swapMem  = Get-WmiObject Win32_PageFileusage | Select-Object *
-        $cacheMem = (Get-Counter '\memory\cache bytes').CounterSamples.CookedValue
+        $cacheMem = (Get-Counter "\$Memory\cache bytes").CounterSamples.CookedValue
         $usedCache = $cacheMem/1GB
         $usedCache = [math]::round($usedCache, 2)
         $totalSwap = $swapMem.AllocatedBaseSize/1000
--- functions/func_top.ps1
+++ functions/func_top.ps1
@@ -19,6 +19,33 @@
   top -EntryNumber 19
 #>
 
+Function Get-PerformanceCounterLocalName
+{
+  param
+  (
+    [UInt32]
+    $ID,
+	$ComputerName = $env:COMPUTERNAME
+  )
+
+  $code = '[DllImport("pdh.dll", SetLastError=true, CharSet=CharSet.Unicode)] public static extern UInt32 PdhLookupPerfNameByIndex(string szMachineName, uint dwNameIndex, System.Text.StringBuilder szNameBuffer, ref uint pcchNameBufferSize);'
+
+  $Buffer = New-Object System.Text.StringBuilder(1024)
+  [UInt32]$BufferSize = $Buffer.Capacity
+
+  $t = Add-Type -MemberDefinition $code -PassThru -Name PerfCounter -Namespace Utility
+  $rv = $t::PdhLookupPerfNameByIndex($ComputerName, $id, $Buffer, [Ref]$BufferSize)
+
+  if ($rv -eq 0)
+  {
+    $Buffer.ToString().Substring(0, $BufferSize-1)
+  }
+  else
+  {
+    Throw 'Get-PerformanceCounterLocalName : Unable to retrieve localized name. Check computer name and performance counter ID.'
+  }
+}
+
 function top {
   [CmdletBinding()]
   param(
@@ -34,8 +61,11 @@ function top {
     while($ProcessCount -le $EntryNumber){
       $Time = Get-Uptime
       $UpTime = "$($Time.Hours)h|$($Time.Minutes)m|$($Time.Seconds)s"
-      $cpuTime = (Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
-      $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
+      $processor = Get-PerformanceCounterLocalName 238
+      $percentProcessorTime = Get-PerformanceCounterLocalName 6
+      $cpuTime = (Get-Counter "\$processor(_total)\$percentProcessorTime").CounterSamples.CookedValue
+      $Memory = Get-PerformanceCounterLocalName 4
+      $availMem = (Get-Counter '\$Memory\Available MBytes').CounterSamples.CookedValue
       $Mem = $availMem.ToString("N0")
       $Mem = $Mem -split ","
       $Mem = $Mem[0]
@@ -86,9 +116,12 @@ function top {
         else{
           while($ProcessCount -le $EntryNumber){
             $Time = Get-Uptime
-            $UpTime = "$($Time.Hours)h|$($Time.Minutes)m|$($Time.Seconds)s"
-            $cpuTime = (Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
-            $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
+          $UpTime = "$($Time.Hours)h|$($Time.Minutes)m|$($Time.Seconds)s"
+          $processor = Get-PerformanceCounterLocalName 238
+          $percentProcessorTime = Get-PerformanceCounterLocalName 6
+          $cpuTime = (Get-Counter "\$processor(_total)\$percentProcessorTime").CounterSamples.CookedValue
+          $Memory = Get-PerformanceCounterLocalName 4
+          $availMem = (Get-Counter '\$Memory\Available MBytes').CounterSamples.CookedValue
             $Mem = $availMem.ToString("N0")
             $Mem = $Mem -split ","
             $Mem = $Mem[0]

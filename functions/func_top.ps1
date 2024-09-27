6ace3113fe48a28b7a20db1d7c287150a9c32b5b
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2024-09-27 21:58:58 +0200

    Update func_top.ps1

--- functions/func_top.ps1
+++ functions/func_top.ps1
@@ -19,33 +19,6 @@
   top -EntryNumber 19
 #>
 
-Function Get-PerformanceCounterLocalName
-{
-  param
-  (
-    [UInt32]
-    $ID,
-	$ComputerName = $env:COMPUTERNAME
-  )
-
-  $code = '[DllImport("pdh.dll", SetLastError=true, CharSet=CharSet.Unicode)] public static extern UInt32 PdhLookupPerfNameByIndex(string szMachineName, uint dwNameIndex, System.Text.StringBuilder szNameBuffer, ref uint pcchNameBufferSize);'
-
-  $Buffer = New-Object System.Text.StringBuilder(1024)
-  [UInt32]$BufferSize = $Buffer.Capacity
-
-  $t = Add-Type -MemberDefinition $code -PassThru -Name PerfCounter -Namespace Utility
-  $rv = $t::PdhLookupPerfNameByIndex($ComputerName, $id, $Buffer, [Ref]$BufferSize)
-
-  if ($rv -eq 0)
-  {
-    $Buffer.ToString().Substring(0, $BufferSize-1)
-  }
-  else
-  {
-    Throw 'Get-PerformanceCounterLocalName : Unable to retrieve localized name. Check computer name and performance counter ID.'
-  }
-}
-
 function top {
   [CmdletBinding()]
   param(
@@ -60,68 +33,62 @@ function top {
   if($elevated){  
     while($ProcessCount -le $EntryNumber){
       $Time = Get-Uptime
-      $UpTime = "$($Time.Hours)h|$($Time.Minutes)m|$($Time.Seconds)s"
-      $processor = Get-PerformanceCounterLocalName 238
-      $percentProcessorTime = Get-PerformanceCounterLocalName 6
-      $cpuTime = (Get-Counter "\$processor(_total)\$percentProcessorTime").CounterSamples.CookedValue
-      $Memory = Get-PerformanceCounterLocalName 4
-      $availMem = (Get-Counter '\$Memory\Available MBytes').CounterSamples.CookedValue
-      $Mem = $availMem.ToString("N0")
-      $Mem = $Mem -split ","
-      $Mem = $Mem[0]
-      $AllMem = $totalRam/1GB
-      $Usage = "Uptime: " + $UpTime + ' > CPU: ' + $cpuTime.ToString("#,0.000") + '% | Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%)'                
-  
-        $processMemoryUsage = Get-WmiObject WIN32_PROCESS | Sort-Object -Property ws -Descending | Select-Object -First $EntryNumber processname, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}
-        $CPUBar = $cpuTime
-        $CPUBar = [math]::Round($CPUBar)
-        $MemBar = $AllMem - $Mem
+    $UpTime = "$($Time.Hours)h|$($Time.Minutes)m|$($Time.Seconds)s"
+    $cpuTime = (Get-WmiObject -Query "SELECT LoadPercentage FROM Win32_Processor WHERE DeviceID='CPU0'").LoadPercentage
+    $availMem = (Get-WmiObject -Query "SELECT FreePhysicalMemory FROM Win32_OperatingSystem").FreePhysicalMemory / 1024
+    $Mem = $availMem.ToString("N0")
+    $Mem = $Mem -split ","
+    $Mem = $Mem[0]
+    $AllMem = $totalRam/1GB
+    $Usage = "Uptime: " + $UpTime + ' > CPU: ' + $cpuTime.ToString("#,0.000") + '% | Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%)'                
 
-        $ProcessList = Get-Process -IncludeUserName | Sort-Object -Descending CPU | Select-Object -First $EntryNumber
+      $processMemoryUsage = Get-WmiObject WIN32_PROCESS | Sort-Object -Property ws -Descending | Select-Object -First $EntryNumber processname, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}
+      $CPUBar = $cpuTime
+      $CPUBar = [math]::Round($CPUBar)
+      $MemBar = $AllMem - $Mem
 
-        if($ProcessList.count -eq $EntryNumber){
-        Clear-Host
+      $ProcessList = Get-Process -IncludeUserName | Sort-Object -Descending CPU | Select-Object -First $EntryNumber
 
-        $CPUi = 0
-        $CPUx = 0
-        foreach($CPUi in $CPUBar){
-            foreach($CPUx in $CPUi){
-            $NoLoad = " "*(100-$CPUx)
-            $Load = "|"*$CPUx
-            }
-        Write-Host "CPU Load: $CPUBar%" -ForegroundColor Green
-        Write-Host -ForegroundColor Yellow "[ $Load $NoLoad ]" -NoNewline
-        }
-        "`n"
-        $Memi = 0
-        $Memx = 0
-        foreach($Memi in $MemBar){
-            foreach($Memx in $Memi){
-            $NoLoad = " "*(100-$Memx)
-            $Load = "|"*$Memx
-            }
-        Write-Host "Mem Load: $Membar%" -ForegroundColor Green
-        Write-Host -ForegroundColor Yellow "[ $Load $NoLoad ]" -NoNewline
-        }
-        "`n"          
-        $Usage; $ProcessList; $processMemoryUsage | Format-Table
+      if($ProcessList.count -eq $EntryNumber){
+      Clear-Host
+
+      $CPUi = 0
+      $CPUx = 0
+      foreach($CPUi in $CPUBar){
+          foreach($CPUx in $CPUi){
+          $NoLoad = " "*(100-$CPUx)
+          $Load = "|"*$CPUx
+          }
+      Write-Host "CPU Load: $CPUBar%" -ForegroundColor Green
+      Write-Host -ForegroundColor Yellow "[ $Load $NoLoad ]" -NoNewline
+      }
+      "`n"
+      $Memi = 0
+      $Memx = 0
+      foreach($Memi in $MemBar){
+          foreach($Memx in $Memi){
+          $NoLoad = " "*(100-$Memx)
+          $Load = "|"*$Memx
+          }
+      Write-Host "Mem Load: $Membar%" -ForegroundColor Green
+      Write-Host -ForegroundColor Yellow "[ $Load $NoLoad ]" -NoNewline
+      }
+      "`n"          
+      $Usage; $ProcessList; $processMemoryUsage | Format-Table
 
 
-        $ProcessList = $null
-        $Usage = $null
-        $processMemoryUsage = $Null
+      $ProcessList = $null
+      $Usage = $null
+      $processMemoryUsage = $Null
         }
       }
         }
         else{
           while($ProcessCount -le $EntryNumber){
             $Time = Get-Uptime
-          $UpTime = "$($Time.Hours)h|$($Time.Minutes)m|$($Time.Seconds)s"
-          $processor = Get-PerformanceCounterLocalName 238
-          $percentProcessorTime = Get-PerformanceCounterLocalName 6
-          $cpuTime = (Get-Counter "\$processor(_total)\$percentProcessorTime").CounterSamples.CookedValue
-          $Memory = Get-PerformanceCounterLocalName 4
-          $availMem = (Get-Counter '\$Memory\Available MBytes').CounterSamples.CookedValue
+            $UpTime = "$($Time.Hours)h|$($Time.Minutes)m|$($Time.Seconds)s"
+            $cpuTime = (Get-WmiObject -Query "SELECT LoadPercentage FROM Win32_Processor WHERE DeviceID='CPU0'").LoadPercentage
+            $availMem = (Get-WmiObject -Query "SELECT FreePhysicalMemory FROM Win32_OperatingSystem").FreePhysicalMemory / 1024
             $Mem = $availMem.ToString("N0")
             $Mem = $Mem -split ","
             $Mem = $Mem[0]
@@ -132,12 +99,12 @@ function top {
               $CPUBar = $cpuTime
               $CPUBar = [math]::Round($CPUBar)
               $MemBar = $AllMem - $Mem
-  
+        
               $ProcessList = Get-Process | Sort-Object -Descending CPU | Select-Object -First $EntryNumber
-
+        
               if($ProcessList.count -eq $EntryNumber){
               Clear-Host
-
+        
               $CPUi = 0
               $CPUx = 0
               foreach($CPUi in $CPUBar){
@@ -161,9 +128,8 @@ function top {
               }
               "`n"          
               $Usage; $ProcessList; $processMemoryUsage | Format-Table
-  
-
-
+        
+        
               $ProcessList = $null
               $Usage = $null
               $processMemoryUsage = $Null
